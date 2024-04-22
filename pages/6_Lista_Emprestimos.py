af7a79b44db58ccaad4e9187e6767d760d07d883
João-Pedro-Queiro <jpqv0105@gmail.com>
2024-04-22 11:14:10 -0300

    Criando filtros no ler empréstimos

--- pages/6_Lista_Emprestimos.py
+++ pages/6_Lista_Emprestimos.py
@@ -7,13 +7,31 @@ st.set_page_config(page_title="Lista Empréstimos")
 
 url = "http://127.0.0.1:5000/emprestimos"
 
+st.title("Empréstimos cadastrados/Meus empréstimos")
+
+st.title("Filtro:")
+
+id_usuario = st.text_input("Id do usuário:", placeholder="Digite um id do usuário...")
+
+id_bicicleta = st.text_input("Id da bicicleta:", placeholder="Digite um id da bicicleta...")
+
+if st.button("Enviar:"):
+    if id_usuario and not id_bicicleta:
+        url += f"?id_usuario={id_usuario}"
+
+    if id_bicicleta and not id_usuario:
+        url += f"?id_bicicleta={id_bicicleta}"
+
+    if id_usuario and id_bicicleta:
+        url += f"?id_usuario={id_usuario}&id_bicicleta={id_bicicleta}"
+
 r = requests.get(url)
 status_code_resposta = r.status_code
 
 if status_code_resposta == 200:
     emprestimos = r.json()["emprestimos"]
 
-    st.title("Empréstimos cadastrados/Meus empréstimos")
+    st.title("Empréstimos cadastrados")
 
     df = pd.DataFrame(emprestimos)
 
