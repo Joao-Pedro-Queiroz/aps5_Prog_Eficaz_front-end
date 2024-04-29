8837ddf37d7b9fb8ffc5a0b9bacadb5ecc0ab4e2
Joao-Pedro-Queiroz <jpqv0105@gmail.com>
2024-04-29 17:26:54 -0300

    tirando aviso do hello.py

--- Hello.py
+++ Hello.py
@@ -9,14 +9,3 @@ url = "http://127.0.0.1:5000/"
 st.title("Empréstimo de bicicletas")
 
 st.write("Fique a vontade para usar nosso site para aluguar uma bicicleta para uso pessoal")
-
-st.markdown(
-    f"""
-    <style>
-    .reportview-container {{
-        background: url(data:image/{main_bg_ext};base64,{base64.b64encode(open(main_bg, "rb").read()).decode()})
-    }}
-    </style>
-    """,
-    unsafe_allow_html=True
-)
